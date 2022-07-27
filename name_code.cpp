ed5bd6759d7c91cf0f3c7395ff5f2439831cf965
Karun Pandit <karunpandit@gmail.com>
2022-07-27 00:49:43 -0400

    Add files via upload

---
+++ name_code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string first_name, last_name;
+    cout<<"First name: ";
+    cin>> first_name ;
+    cout<<"last name: ";
+    cin>>last_name;
+    cout<<"so you're "<<first_name+" "+last_name;
+    return 0;
+} l
